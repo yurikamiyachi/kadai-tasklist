b20e3c4ddf4a2a2c34fb6c5c92faeadd56e31689
yuri.miya <yuri.miya@yahoo.co.jp>
2020-12-19 00:19:18 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -38,8 +38,9 @@ public class EditServlet extends HttpServlet {
 
         request.setAttribute("task", m);
         request.setAttribute("_token",request.getSession().getId());
-
-        request.getSession().setAttribute("task_id",m.getId());
+        if(m != null){
+            request.getSession().setAttribute("task_id",m.getId());
+        }
 
         RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
