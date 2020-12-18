f0d27767e33ae587e8e95523dd877d4146998116
yuri.miya <yuri.miya@yahoo.co.jp>
2020-12-18 23:29:53 +0900

    Add Updatedservlet.java

--- WebContent/WEB-INF/views/tasks/edit.jsp
+++ WebContent/WEB-INF/views/tasks/edit.jsp
@@ -1,11 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>Insert title here</title>
-    </head>
-    <body>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="../layout/app.jsp">
+    <c:param name="content">
+        <h2>id : ${task.id} のメッセージ編集ページ</h2>
 
-    </body>
-</html>
+        <form method="POST" action="${pageContext.request.contextPath}/update">
+            <c:import url="_form.jsp" />
+        </form>
+
+        <p><a href="${pageContext.request.contextPath}/index">一覧に戻る</a></p>
+
+    </c:param>
+</c:import>
--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Task;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class EditServlet
  */
 @WebServlet(name = "edit", urlPatterns = { "/edit" })
 public class EditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +28,21 @@ public class EditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+        Task m = em.find(Task.class,Integer.parseInt(request.getParameter("id")));
+        em.close();
+
+        request.setAttribute("task", m);
+        request.setAttribute("_token",request.getSession().getId());
+
+        request.getSession().setAttribute("task_id",m.getId());
+
+        RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
+    }
 
 }
